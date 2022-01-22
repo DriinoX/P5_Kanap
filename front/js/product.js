604a2678abe868f8e58cfb351678ebceac8a252c
Hadrien Vaison <hadrien@macbook-pro-de-hadrien-2.home>
2022-01-22 19:04:40 +0100

    add change

--- front/js/product.js
+++ front/js/product.js
@@ -9,25 +9,18 @@ fetch("http://localhost:3000/api/products/" + id)
   })
   .then(function(value) {
     console.log(value);
-
     var item_img = document.querySelector(".item__img");
     var img = document.createElement("img");
-
     var title = document.querySelector("#title");
     var price = document.querySelector("#price");
     var description = document.querySelector("#description");
     var color = document.querySelector("#colors");
-
     item_img.appendChild(img);
     img.setAttribute('src',value.imageUrl);
     img.setAttribute('alt', value.altTxt);
-
     title.innerText = value.name;
-
     price.innerText = value.price;
-
     description.innerText = value.description;
-
     for (let i = 0; i < value.colors.length; i++) {
     	var option = document.createElement("option");
     	option.setAttribute('value', value.colors[i]);
