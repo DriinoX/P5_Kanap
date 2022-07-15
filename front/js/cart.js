f44765208ed6746273bfaa3c4588d1cfa25d2d51
Hadrien Vaison <hadrien@macbook-pro-de-hadrien-2.home>
2022-07-15 13:33:41 +0200

    post

--- front/js/cart.js
+++ front/js/cart.js
@@ -5,65 +5,132 @@ let totalPrice = document.querySelector("#totalPrice");
 products = getCart()
 sumQuantity = 0;
 sumPrice = 0;
-products.forEach((product) => {
 
-  fetch("http://localhost:3000/api/products/" + product.id)
-    .then(function(res) {
-      if (res.ok) {
-        return res.json();
-      }
-    })
-    .then(function(value) {
-    	imageUrl = value.imageUrl
-		let cart__item = document.createElement("article");
-		cart__item.setAttribute('data-id',product.id);
-		cart__item.setAttribute('data-color',product.color);
-		cart__item.setAttribute('class','cart__item');
-		cart__item.innerHTML = `
-		<div class='cart__item__img'>
-		  <img src='${value.imageUrl}' alt='Photographie d'un canapé'>
-		</div>
-		<div class='cart__item__content'>
-		  <div class='cart__item__content__description'>
-		    <h2>${product.title}</h2>
-		    <p>${product.color}</p>
-		    <p>${value.price} €</p>
-		  </div>
-		  <div class='cart__item__content__settings'>
-		    <div class='cart__item__content__settings__quantity'>
-		      <p>Qté : </p>
-		      <input type='number' class='itemQuantity' name='itemQuantity' min='1' max='100' value='${product.quantity}'>
-		    </div>
-		    <div class='cart__item__content__settings__delete'>
-		      <p class='deleteItem'>Supprimer</p>
-		    </div>
-		  </div>
-		</div>`
-		// ajout de l'article dans la div cart__items
-		cart__items.appendChild(cart__item)
-		displayTotalPrice(value.price, product.quantity)
+// find avec tout les produit avec les present dans le panier pour les ajouter
+// products.forEach((product) => {
 
-		// addeventlistener sur les select quantité
-		const itemQuantitys = document.querySelectorAll('.itemQuantity');
-		itemQuantitys.forEach(itemQuantity => {
-		  itemQuantity.addEventListener('change', event => {
-		    actualisationValues()
-		  });
-		});
+//   fetch("http://localhost:3000/api/products/" + product.id)
+//     .then(function(res) {
+//       if (res.ok) {
+//         return res.json();
+//       }
+//     })
+//     .then(function(value) {
+//     	imageUrl = value.imageUrl
+// 		let cart__item = document.createElement("article");
+// 		cart__item.setAttribute('data-id',product.id);
+// 		cart__item.setAttribute('data-color',product.color);
+// 		cart__item.setAttribute('class','cart__item');
+// 		cart__item.innerHTML = `
+// 		<div class='cart__item__img'>
+// 		  <img src='${value.imageUrl}' alt='Photographie d'un canapé'>
+// 		</div>
+// 		<div class='cart__item__content'>
+// 		  <div class='cart__item__content__description'>
+// 		    <h2>${product.title}</h2>
+// 		    <p>${product.color}</p>
+// 		    <p>${value.price} €</p>
+// 		  </div>
+// 		  <div class='cart__item__content__settings'>
+// 		    <div class='cart__item__content__settings__quantity'>
+// 		      <p>Qté : </p>
+// 		      <input type='number' class='itemQuantity' name='itemQuantity' min='1' max='100' value='${product.quantity}'>
+// 		    </div>
+// 		    <div class='cart__item__content__settings__delete'>
+// 		      <p class='deleteItem'>Supprimer</p>
+// 		    </div>
+// 		  </div>
+// 		</div>`
+// 		// ajout de l'article dans la div cart__items
+// 		cart__items.appendChild(cart__item)
+// 		displayTotalPrice(value.price, product.quantity)
 
-		// addeventlistener sur les btn delete
-		let deleteItems = document.querySelectorAll('.deleteItem');
-		for (var i = 0; i < deleteItems.length; i++) {
-			// deleteItems = document.querySelectorAll('.deleteItem');
-			deleteItems[i].addEventListener('click', event => {
-		    	deleteProduct(deleteItems[i])
-		    	console.log(deleteItems[i])
-		  });
+// 		// addeventlistener sur les select quantité
+// 		const itemQuantitys = document.querySelectorAll('.itemQuantity');
+// 		itemQuantitys.forEach(itemQuantity => {
+// 		  itemQuantity.addEventListener('change', event => {
+// 		    actualisationValues()
+// 		  });
+// 		});
+
+// 		// addeventlistener sur les btn delete
+// 		let deleteItems = document.querySelectorAll('.deleteItem');
+// 		for (var i = 0; i < deleteItems.length; i++) {
+// 			// deleteItems = document.querySelectorAll('.deleteItem');
+// 			deleteItems[i].addEventListener('click', event => {
+// 		    	deleteProduct(deleteItems[i])
+// 		  });
+// 		}
+//     });
+//     sumQuantity = sumQuantity + product.quantity
+// });
+// totalQuantity.innerText = sumQuantity;
+
+fetch("http://localhost:3000/api/products")
+.then(function(res) {
+  if (res.ok) {
+    return res.json();
+  }
+})
+.then(function(values) {
+	products = getCart()
+	let id_array = []
+	values.forEach((value) => {
+		id_array += value._id
+	});
+	products.forEach((product, index) => {
+		if (id_array.includes(product.id)) {
+			fetch("http://localhost:3000/api/products/" + product.id)
+			    .then(function(res) {
+			      if (res.ok) {
+			        return res.json();
+			      }
+			    })
+			    .then(function(value) {
+			    	imageUrl = value.imageUrl
+					let cart__item = document.createElement("article");
+					cart__item.setAttribute('data-id',product.id);
+					cart__item.setAttribute('data-color',product.color);
+					cart__item.setAttribute('class','cart__item');
+					cart__item.innerHTML = `
+					<div class='cart__item__img'>
+					  <img src='${value.imageUrl}' alt='Photographie d'un canapé'>
+					</div>
+					<div class='cart__item__content'>
+					  <div class='cart__item__content__description'>
+					    <h2>${product.title}</h2>
+					    <p>${product.color}</p>
+					    <p>${value.price} €</p>
+					  </div>
+					  <div class='cart__item__content__settings'>
+					    <div class='cart__item__content__settings__quantity'>
+					      <p>Qté : </p>
+					      <input type='number' class='itemQuantity' name='itemQuantity' min='1' max='100' value='${product.quantity}'>
+					    </div>
+					    <div class='cart__item__content__settings__delete'>
+					      <p class='deleteItem'>Supprimer</p>
+					    </div>
+					  </div>
+					</div>`
+					// ajout de l'article dans la div cart__items
+					cart__items.appendChild(cart__item)
+					displayTotalPrice(value.price, product.quantity)
+
+					const itemQuantitys = document.querySelectorAll('.itemQuantity');
+					itemQuantitys[index].addEventListener('change', event => {
+					    actualisationValues()
+					});
+
+					// addeventlistener sur les btn delete
+					let deleteItems = document.querySelectorAll('.deleteItem');
+					deleteItems[index].addEventListener('click', event => {
+				    	deleteProduct(deleteItems[index])
+				  	});
+			    })
 		}
-    });
-    sumQuantity = sumQuantity + product.quantity
+	});
 });
-totalQuantity.innerText = sumQuantity;
+
 
 function displayTotalPrice(valuePrice, productQuantity) {
     sumPrice = sumPrice + valuePrice * productQuantity
@@ -97,12 +164,10 @@ function actualisationValues() {
 }
 
 function deleteProduct(element) {
-	console.log(element)
 	article = element.parentNode.parentNode.parentNode.parentNode
 	product = {id: article.dataset.id, color: article.dataset.color}
-	console.log(product)
-	// removeToCart(product)
-	// location.href = window.location.href
+	removeToCart(product)
+	location.href = window.location.href
 }
 
 function saveCart(cart) {
@@ -124,14 +189,6 @@ function removeToCart(product) {
   saveCart(cart);
 }
 
-
-// addeventlistener sur le btn order
-let btn_order = document.querySelector("#order")
-btn_order.addEventListener('click', event => {
-  verificationValues()
-  // actualisationLocalStorage()
-});
-
 // Actualisation du localStorage
 function actualisationLocalStorage() {
   let cart = getCart();
@@ -141,45 +198,102 @@ function actualisationLocalStorage() {
 }
 
 // verification des champs du contact
-function verificationValues() {
-	console.log('test')
-	let firstName = document.querySelector("#firstName")
-	let lastName = document.querySelector("#lastName")
-	let address = document.querySelector("#address")
-	let city = document.querySelector("#city")
-	let email = document.querySelector("#email")
+	// pas de chiffre dans ces champs
+let firstName = document.querySelector('#firstName');
+let lastName = document.querySelector('#lastName');
+let city = document.querySelector('#city');
+firstName.addEventListener('change', event => {
+	verificationNoNumber(firstName)
+});
+lastName.addEventListener('change', event => {
+	verificationNoNumber(lastName)
+});
+city.addEventListener('change', event => {
+	verificationNoNumber(city)
+});
+	// pas de restriction sur l'addresse
+let address = document.querySelector('#address');
+address.addEventListener('change', event => {
+	verificationAddress(address)
+});
+	// verification format de l'adresse mail
+let email = document.querySelector('#email');
+email.addEventListener('change', event => {
+	verificationEmail(email)
+});
 
-	// /[a-zA-Z\-a-zA-Z]+/gm
-	let regex1 = new RegExp('/[a-zA-Z]+/gm')
-	// verif email /\w*[@]\w*\.com/gm
-	let regex2 = new RegExp('/\w*[@]\w*\.com/gm')
-	let verif1 = false
-	let verif2 = false
-	let verif3 = false
-
-	if (regex1.test(firstName.value) && regex1.test(lastName.value) && regex1.test(city.value)) {
-		verif1 = true
+function verificationNoNumber(element) {
+	let regex1 = /\d+/
+	let message = document.querySelector("#" + element.id + "ErrorMsg")
+	if (element.value != "") {
+		if (regex1.test(element.value)) {
+			message.innerText = "Ce champs ne doit pas contenir de chiffre !"
+		} else {
+			message.innerText = ""
+		}
+	} else {
+		message.innerText = "Ce champs ne doit pas etre vide !"
 	}
-
-	if (regex2.test(email.value)) {
-		verif2 = true
-	}
-
-	if (address.value != "") {
-		verif3 = true
-	}
-
-	if (verif1 && verif2 && verif3) {
-		console.log('verification OK!')
-	}
-
-	// si toutes les verif sont passées alors appeler la fonction order
 }
-// message d'erreur direct
 
-// si un message d'erreur pas redirection avec le btn, sinon page commande
+function verificationAddress(element) {
+	let message = document.querySelector("#" + element.id + "ErrorMsg")
+	if (element.value != "") {
+		message.innerText = ""
+	} else {
+		message.innerText = "Ce champs ne doit pas etre vide !"
+	}
+}
+
+function verificationEmail(element) {
+	let regex1 = /\w*[@]\w*\.com/
+	let message = document.querySelector("#" + element.id + "ErrorMsg")
+	if (element.value != "") {
+		if (!regex1.test(element.value)) {
+			message.innerText = "Ce champs doit avoir le format d'une adresse mail !"
+		} else {
+			message.innerText = ""
+		}
+	} else {
+		message.innerText = "Ce champs ne doit pas etre vide !"
+	}
+}
+
+// addeventlistener sur le btn order
+let btn_order = document.querySelector("#order")
+btn_order.addEventListener('click', event => {
+    let firstNameErrorMsg = document.querySelector("#firstNameErrorMsg")
+  	let lastNameErrorMsg = document.querySelector("#lastNameErrorMsg")
+  	let cityErrorMsg = document.querySelector("#cityErrorMsg")
+  	let addressErrorMsg = document.querySelector("#addressErrorMsg")
+  	let emailErrorMsg = document.querySelector("#emailErrorMsg")
+  	if (firstNameErrorMsg.innerText == "" && lastNameErrorMsg.innerText == "" && cityErrorMsg.innerText == "" && addressErrorMsg.innerText == "" && emailErrorMsg.innerText == "" && emailErrorMsg.innerText == "") {
+  		// post la commande puis redirection avec le numero de commande du resultat
+  		order()
+  	} else {
+  		console.log("not order")
+  		// location.href = window.location.href
+  	}
+});
 
 // envoi des infos(contact + commande) a l'API + redirection vers confirmation page avec l'id commande récuperer via la method post
 function order() {
+	let firstName = document.querySelector("#firstName")
+	let lastName = document.querySelector("#lastName")
+	let city = document.querySelector("#city")
+	let address = document.querySelector("#address")
+	let email = document.querySelector("#email")
 
+	contact = {firstName: firstName.value, lastName: lastName.value, address: address.value, city: city.value, email: email.value}
+	console.log(contact)
+	products = getCart()
+	jsonBody = [contact, products]
+	fetch("http://localhost:3000/api/products/order", {
+		method: "POST",
+		headers: { 
+	'Accept': 'application/json', 
+	'Content-Type': 'application/json' 
+	},
+		body: JSON.stringify(jsonBody)
+	});
 }
